32e3a0e0287d86a0783add3dabd656062803cd32
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-08-21 08:57:45 +0000

    a little light documentinfg
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Net-Rendezvous-Native@2831 c56e198b-7db7-0310-b3ab-8e406f684e33

--- lib/Net/Rendezvous/Native.pm
+++ lib/Net/Rendezvous/Native.pm
@@ -1,23 +1,25 @@
 package Net::Rendezvous::Native;
-
 use strict;
-#use warnings;
-
-
+use warnings;
+our $VERSION = '0.01';
 
 =head1 NAME
 
-Net::Rendezvous::Native - 
+Net::Rendezvous::Native - Use native rendezvous library, if available
 
 =head1 SYNOPSIS
 
-  use Net::Rendezvous::Native;
-
+ use Net::Rendezvous::Native;
+ my $res = Net::Rendezvous->new( $service, );
+ $res->discover;
 
 
 =head1 DESCRIPTION
 
-
+Net::Rendezvous::Native will augment Net::Rendezvous to use a native
+Rendezvous library, if available.  The native versions are typically
+quicker to query, and more robust than doing it by hand as
+Net::Rendezvous does.
 
 =cut
 
@@ -31,13 +33,12 @@ Richard Clamp <richardc@unixbeard.net>
 
 Copyright 2004 Richard Clamp.  All Rights Reserved.
 
-This program is free software; you can redistribute it
-and/or modify it under the same terms as Perl itself.
-
-
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
+Net::Rendezvous
 
 =cut
 
