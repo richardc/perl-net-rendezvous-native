2465cfbac46013625b6724dbf9b02b4d5475e9a6
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-08-21 08:51:43 +0000

    yay for annoyance
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Net-Rendezvous-Native@2830 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Net/Rendezvous/Native.pm
@@ -0,0 +1,44 @@
+package Net::Rendezvous::Native;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Net::Rendezvous::Native - 
+
+=head1 SYNOPSIS
+
+  use Net::Rendezvous::Native;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 COPYRIGHT
+
+Copyright 2004 Richard Clamp.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
